82c724eb85ec560ed6cc06dd5e48e879db07fa04
wissam alnahhal <exewesam1@gmail.com>
2023-01-25 08:05:08 +0200

    first commit

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const storeProducts = [
   {
     id: 1,
     title: "Google Pixel - Black",
-    img: "public/img/product-1.png",
+    img: "../public/img/product-1.png",
     price: 10,
     company: "GOOGLE",
     info:
